c7c23bb290467dfe443c3dda8b26ee27ed617862
Yi Hong-ju <to_hongju@pm.me>
2024-10-25 15:52:11 +0900

    Update Google Drive file download logic
    하나의 작품 데이터가 여러개의 이미지 파일로 이루어진 경우에 대응할 수 있도록 파일 다운로드 로직을 업데이트 하였습니다.
    Artwork DTO에서 작품 데이터를 담는 부분 또한 리스트로 변경되었습니다.

--- data/artwork.py
+++ data/artwork.py
@@ -11,65 +11,81 @@ from dtos import Artwork
 
 class ArtworkDataPipeline:
     def __init__(self) -> None:
-        self.service_account_file = 'service_account_key.json'
         self.spreadsheet_id = os.getenv('SPREADSHEET_ID')
+        self.service_account_file = 'service_account_key.json'
         self.creds = Credentials.from_service_account_file(
             self.service_account_file)
+
         self.sheets_service = build('sheets', 'v4', credentials=self.creds)
-        self.sheet = self.sheets_service.spreadsheets()  # pylint: disable=no-member
         self.drive_service = build('drive', 'v3', credentials=self.creds)
 
-    def __get_new_artworks(self) -> list[Artwork | None]:
-        artworks = []
-        sheet_range = '시트1!B:H'
+        self.sheet = self.sheets_service.spreadsheets()  # pylint: disable=no-member
+        self.artworks = []
 
+    def __get_new_artworks(self) -> bool:
+        sheet_range = '시트1!B:H'
         result = self.sheet.values().get(spreadsheetId=self.spreadsheet_id,
                                          range=sheet_range).execute()
 
-        for row_idx, row in enumerate(result['values'][2:]):  # 0, 1번째 row는 패스
+        for idx, row in enumerate(result['values'][2:]):  # 0, 1번째 row는 패스
             if row[-1] == 'Y':
                 continue  # 이미 적재된 작품
 
             if row[-1] != '정상':
                 continue  # 검수 미완료 데이터
 
-            artworks.append(
-                Artwork(
-                    row_num=row_idx + 1,  # 시트 내 행의 번호
-                    artist_name=row[0],
-                    artwork_name=row[1],
-                    drive_url=row[4],
-                    file_stream=io.BytesIO()  # Dummy instance
-                )
+            artwork = Artwork(
+                row_num=idx + 1,
+                artist_name=row[0],
+                artwork_name=row[1],
+                drive_url=row[4],
+                image_streams=[]  # Dummy
             )
+            self.artworks.append(artwork)
 
-        return artworks
+        if not self.artworks:
+            return False  # No artworks to update
 
-    def __get_artwork_files(self, artworks: list[Artwork]) -> None:
-        for artwork in artworks:
-            # Get file's ID from full URL
-            file_id = artwork.drive_url.replace(
-                'https://drive.google.com/file/d/', '')
-            file_id = file_id.replace('/view?usp=drive_link', '')
+        return True
 
-            request = self.drive_service.files().get_media(  # pylint: disable=no-member
-                fileId=file_id)
-            downloader = MediaIoBaseDownload(artwork.file_stream, request)
+    def __get_artwork_files(self) -> None:
+        for artwork in self.artworks:
+            folder_id = artwork.drive_url.replace(
+                'https://drive.google.com/drive/folders/', '')
+            folder_id = folder_id.replace('?usp=drive_link', '')
+            q = f'\'{folder_id}\' in parents and trashed = false'
 
-            while True:
-                _, done = downloader.next_chunk()  # TODO: 다운로드 API 호출 예외처리 (timeout, etc.)
+            files = self.drive_service.files().list(  # pylint: disable=no-member
+                q=q,
+                orderBy='name_natural'
+            ).execute()['files']
 
-                if done is True:
-                    break
+            for file in files:
+                request = self.drive_service.files().get_media(  # pylint: disable=no-member
+                    fileId=file['id'])
+                image_stream = io.BytesIO()
+                downloader = MediaIoBaseDownload(image_stream, request)
+
+                logging.info('Downloading %s ... 0%%', file['name'])
+
+                while True:
+                    # TODO: 다운로드 API 호출 예외처리 (timeout, etc.)
+                    status, done = downloader.next_chunk()
+
+                    logging.info('Downloading %s ... %d%%',
+                                 file['name'], int(status.progress() * 100))
+
+                    if done is True:
+                        break
+
+                artwork.image_streams.append(image_stream)
 
     def activate_pipeline(self) -> bool:
-        artworks = self.__get_new_artworks()
-
-        if not artworks:
+        if not self.__get_new_artworks():
             logging.info('새로 적재할 artwork가 없습니다.')
             return True
 
-        self.__get_artwork_files(artworks)
+        self.__get_artwork_files()
 
         # TODO
         # self.__load_artwork_files_to_bucket()
