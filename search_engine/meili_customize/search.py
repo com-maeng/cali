ad9ab9109ea8ffca4007ec9536948de55cd1357e
AMinSC <cdtjdcjf@gmail.com>
2024-11-22 20:42:43 +0900

    Add search func in meili customize, Add search test

--- search_engine/meili_customize/search.py
+++ search_engine/meili_customize/search.py
@@ -18,7 +18,6 @@ class Search:
 
     def __init__(self, config: Config):
         self.config = config
-        self.five_style = ['jeonseo', 'yeseo', 'haeseo', 'haengseo', 'choseo']
 
     def search(self, chi: str, s_val: str = 'jeonseo') -> list[dict]:
         """사용자가 입력한 문자열 'chi'와 's_val' 기준으로 필터링된 정보를 반환합니다.
@@ -30,18 +29,9 @@ class Search:
             chi: str ['한자', '훈', '음', '훈 음' 모두 검색 가능 (예: '月 달 월')]
             s_val: str [오서('jeonseo', 'yeseo', 'haeseo', 'haengseo', 'choseo') 중 한개를 입력하며, 기본값은 'jeonseo'입니다.]
 
-        Raises:
-            ValueError: 인자가 조건에 맞지 않는 경우
-
         Returns:
             hits: 검색 조건에 맞는 한자 문서를 반환
         """
-
-        if s_val not in self.five_style:
-            raise Exception(
-                '오서("jeonseo", "yeseo", "haeseo", "haengseo", "choseo") 중 한개를 입력해주세요.')
-        if not chi and isinstance(chi, str):
-            raise Exception('알맞은 검색어로 검색해주세요.')
 
         filter_str = f'style = "{s_val}"'
         results = self.config.index.search(
@@ -52,7 +42,7 @@ class Search:
         )
         return results
 
-    def suggest(self, chi: str, s_val: str = 'jeonseo') -> list[dict]:
+    def suggest(self, chi: str, s_val: str = 'jeonseo') -> list[str]:
         """사용자가 실시간으로 입력한 문자열 'chi'기반으로 실시간 추천 검색 리스트 5개를 반환합니다.
 
         사용자가 문자열을 입력할 경우, 입력한 문자(or문자열)이 실시간으로 매개변수 'chi'로 요청하여,
@@ -62,18 +52,9 @@ class Search:
             chi: str ['한자', '훈', '음', '훈 음' 모두 검색 가능 (예: '月 달 월')]
             s_val: str [오서('jeonseo', 'yeseo', 'haeseo', 'haengseo', 'choseo') 중 한개를 입력하며, 기본값은 'jeonseo'입니다.]
 
-        Raises:
-            ValueError: 인자가 조건에 맞지 않는 경우
-
         Returns:
             hits: 검색 조건에 맞는 한자 리스트를 반환
         """
-
-        if s_val not in self.five_style:
-            raise Exception(
-                '오서("jeonseo", "yeseo", "haeseo", "haengseo", "choseo") 중 한개를 입력해주세요.')
-        if not chi and isinstance(chi, str):
-            raise Exception('알맞은 검색어로 검색해주세요.')
 
         filter_str = f"style = '{s_val}'"
         results = self.config.index.search(
