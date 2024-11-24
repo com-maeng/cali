131732540f48cfac44f25af420c999c1131e720f
AMinSC <cdtjdcjf@gmail.com>
2024-11-24 19:02:25 +0900

    Add search func in meili customize, Add search test

--- search_engine/tests/test_search.py
+++ search_engine/tests/test_search.py
@@ -17,49 +17,22 @@ class TestSearchMock(unittest.TestCase):
     def test_search(self):
         """검색 테스트"""
         mock_results = {
-            'estimatedTotalHits': 1,
-            'hits': [{'id': 12, 'character': '火 불 화', 'style': 'yeseo'}],
+            'estimatedTotalHits': 5,
+            'hits': [
+                {'id': 10, 'character': '火 불 화', 'style': 'jeonseo'},
+                {'id': 11, 'character': '火 불 화', 'style': 'yeseo'},
+                {'id': 12, 'character': '火 불 화', 'style': 'haeseo'},
+                {'id': 13, 'character': '火 불 화', 'style': 'haengseo'},
+                {'id': 14, 'character': '火 불 화', 'style': 'choseo'},
+            ],
             'limit': 20, 'offset': 0, 'processingTimeMs': 0, 'query': '불'}
 
         self.mock_index.search.return_value = mock_results
-        result = self.search_instance.search('불', 'yeseo')
+        result = self.search_instance.search('불')
 
         # 검색 결과가 잘 나오는지 확인
         self.assertEqual(result, mock_results)
 
         # index.search() 메서드의 인자값으로 아래와 같이 요청이 되었는지 확인
         self.mock_index.search.assert_called_once_with(
-            '불', {'filter': ['style = "yeseo"']})
-
-    def test_suggest(self):
-        """추천 검색어 리스트 테스트"""
-        mock_suggestions = {
-            'hits': [
-                {
-                    'id': 7,
-                    'character': '月 달 월',
-                    'style': 'yeseo'
-                },
-                {
-                    'id': 2,
-                    'character': '日 날 일',
-                    'style': 'yeseo'
-                }
-            ],
-            'query': '달',
-            'processingTimeMs': 367,
-            'limit': 5,
-            'offset': 0,
-            'estimatedTotalHits': 2
-        }
-        self.mock_index.search.return_value = mock_suggestions
-
-        suggestions = self.search_instance.suggest('달', 'yeseo')
-        expected_suggestions = ['月 달 월', '日 날 일']
-
-        # 문자('달') 기반으로 추천 리스트 확인
-        self.assertEqual(suggestions, expected_suggestions)
-
-        # index.search() 메서드의 인자값으로 아래와 같이 요청이 되었는지 확인
-        self.mock_index.search.assert_called_once_with(
-            '달', {'filter': ["style = 'yeseo'"], 'limit': 5})
+            '불', {'limit': 60})
