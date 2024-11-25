1f06b397bd2d6a65c42a6a76fc97a0fc45beecd4
AMinSC <cdtjdcjf@gmail.com>
2024-11-26 00:23:02 +0900

    Django initial settings

--- backend/cali_backend/settings.py
+++ backend/cali_backend/settings.py
@@ -16,6 +16,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 
+load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-637t=(5msymtqj^ln&vx6hgfxm%e31kvh%m^ebhw2^^a-&b(k^'
+SECRET_KEY = os.getenv(
+    'DJANGO_KEY') or 'django-insecure-637t=(5msymtqj^ln&vx6hgfxm%e31kvh%m^ebhw2^^a-&b(k^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.getenv('DEBUG'):
@@ -46,6 +50,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+
+    # app
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +67,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'cali_backend.urls'
 
 TEMPLATES = [
     {
@@ -76,7 +85,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'cali_backend.wsgi.application'
 
 
 # Database
@@ -111,30 +120,23 @@ CACHES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
