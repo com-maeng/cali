4c3e721080134bd411c514f394ef3d16ac037248
Yi Hong-ju <to_hongju@pm.me>
2024-12-13 14:37:22 +0900

    Fix Service Account Key's path
    Secret Manager의 Secret 마운트 시 덮어쓰기 이슈에 대응합니다.

--- data/run_dev_container.sh
+++ data/run_dev_container.sh
@@ -4,6 +4,6 @@ docker build --no-cache -t cali-data-dev -f ./data/Dockerfile.dev ./data
 
 docker run -it \
     --env-file ./.env.dev \
-    --volume ./service_account_key_dev.json:/cali/service_account_key_dev.json:ro \
+    --volume ./keys/service_account_key_dev.json:/cali/keys/service_account_key_dev.json:ro \
     --volume ./data:/cali/data \
     cali-data-dev
