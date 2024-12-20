e73150f008a738ec35e8c307dd72e1d74296d340
Yi Hong-ju <to_hongju@pm.me>
2024-12-20 23:48:07 +0900

    Add VisionAPIClient with text_detection() method

--- data/client/google_client.py
+++ data/client/google_client.py
@@ -2,6 +2,7 @@ import os
 import io
 from typing import NoReturn
 
+from google.cloud import vision
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
@@ -38,3 +39,39 @@ class StorageClient:
                 'name': file_name
             }
         ).execute()
+
+
+class VisionAPIClient:
+    def __init__(self):
+        self.service_account_file = '/'.join([
+            os.getenv('SECRETS_DIR'),
+            f'{os.getenv('SERVICE_ACCOUNT_KEY_NAME')}.json'
+        ])
+        self.creds = Credentials.from_service_account_file(
+            self.service_account_file)
+        self.image_annotator_client = vision.ImageAnnotatorClient(
+            credentials=self.creds)
+
+    def text_detection(self, image: vision.Image) -> str:
+        boxes = ''
+        response = self.image_annotator_client.text_detection(  # pylint: disable=no-member
+            image=image)
+
+        # Parse and return boxes
+        for text in response.text_annotations:
+            if len(text.description) > 1:  # Entire text
+                continue
+
+            # TODO: Is text.description value a chinese character?
+            recognized_character = text.description
+            x1 = text.bounding_poly.vertices[0].x
+            y1 = text.bounding_poly.vertices[0].y
+            x2 = text.bounding_poly.vertices[2].x
+            y2 = text.bounding_poly.vertices[2].y
+            dummy_page_num = '0'
+
+            boxes += ', '.join([recognized_character, str(x1),
+                               str(y1), str(x2), str(y2), dummy_page_num])
+            boxes += '\n'
+
+        return boxes
