e4c1c49bec01695e2581da3203b7d49706dc31e2
Yi Hong-ju <to_hongju@pm.me>
2024-12-22 18:19:42 +0900

    Init iOS application codebase `ios` (template)

---
+++ ios/Cali/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cali
+//
+//  Created by Yi Hong-ju on 12/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
