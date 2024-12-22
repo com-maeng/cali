0e05e8b79b3e6b74491825a140fcdaac7354de70
AMinSC <cdtjdcjf@gmail.com>
2024-12-22 22:43:11 +0900

    DB, devtools 의존성 추가

--- backend/src/main/java/com/cali/CaliApplication.java
+++ backend/src/main/java/com/cali/CaliApplication.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @SpringBootApplication
 @RestController
 public class CaliApplication {
@@ -16,9 +15,8 @@ public class CaliApplication {
 	}
 
 	@GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-    }
-	
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello~ %s!", name);
+	}
 
 }
