947af70e8ca70f6ced28c8a705fd348db89105d6
Yi Hong-ju <to_hongju@pm.me>
2024-12-22 18:19:42 +0900

    Init iOS application codebase `ios` (template)

---
+++ ios/Cali/CaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaliApp.swift
+//  Cali
+//
+//  Created by Yi Hong-ju on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct CaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
