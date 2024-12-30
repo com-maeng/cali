4c380caa1668aa51f5a475763a70493332961eb6
AMinSC <cdtjdcjf@gmail.com>
2024-12-30 23:55:31 +0900

    Removed dummy data, json dependency

--- backend/src/test/java/com/cali/CaliApplicationTests.java
+++ backend/src/test/java/com/cali/CaliApplicationTests.java
@@ -2,15 +2,9 @@ package com.cali;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
-import com.cali.meilisearch.MeilisearchConfig;
 
 @SpringBootTest
 class CaliApplicationTests {
-
-	@MockitoBean
-	private MeilisearchConfig sMeilisearchConfig;
 
 	@Test
 	void contextLoads() {
