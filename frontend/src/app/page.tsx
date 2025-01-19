f4259aa35cb9f1490365db07361371d3a88cdb5a
Yi Hong-ju <to_hongju@pm.me>
2025-01-19 16:05:30 +0900

    Add Link component to home page w/ minor styling

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,46 +1,44 @@
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="w-[530] h-dvh flex justify-center items-center bg-[#f7f8f9]">
       <main className="w-full h-full flex flex-col items-center justify-center">
-        <Image 
-          src="/cali_logo.svg" 
-          alt="캘리" 
+        <Image
+          src="/cali_logo.svg"
+          alt="캘리"
           width={111}
           height={54}
           priority={true}
           className=""
         />
         <div className="w-[80%] my-11">
-          <div className="relative flex items-center">
-            <input 
+          <Link href="/search" className="relative flex items-center h-12">
+            <Image
+              src="/search_logo.svg"
+              alt="검색"
+              width={22}
+              height={22}
+              className="absolute ml-5"
+            />
+            <input
               type="text"
               placeholder="한글 또는 한자를 입력하세요"
-              className="w-full py-[1.4vh] px-[5vh] rounded-full border-none text-[1.6vh] placeholder:text-[#ADB5BD] shadow-[0_2px_8px_rgba(0,0,0,0.08)] focus:outline-none"
+              className="w-full py-3.5 pl-[54px] rounded-full border-none text-base placeholder:text-[#ADB5BD] shadow-[0_2px_8px_rgba(0,0,0,0.08)] focus:outline-none"
             />
-            <div className="absolute left-[2vh]">
-              <Image 
-                src="/search_logo.svg" 
-                alt="검색" 
-                width={22}
-                height={22}
-              />
-            </div>
-            {/* <div className="absolute right-[2vh]">
-              <Image 
-                src="/camera_logo.svg" 
-                alt="카메라" 
-                width={22}
-                height={22}
-              />
-            </div> */}
-          </div>
+          </Link>
+          {/* <Link // Will be implemented later
+            src="/camera_logo.svg"
+            alt="카메라"
+            width={22}
+            height={22}
+          ></Link> */}
         </div>
         <div className="top-[52.21vh] flex flex-col items-center w-full">
-          <Image 
-            src="/home_deco_jeonseo.svg" 
-            alt="데코_전서체" 
+          <Image
+            src="/home_deco_jeonseo.svg"
+            alt="데코_전서체"
             width={50}
             height={50}
             className="mb-1"
