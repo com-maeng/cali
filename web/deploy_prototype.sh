4c2a2d3c957d9d30072c54fdfba5b1d89c30c3c1
Yi Hong-ju <to_hongju@pm.me>
2025-02-08 16:24:39 +0900

    Add force remove option to Dockerfile for prototype

--- web/deploy_prototype.sh
+++ web/deploy_prototype.sh
@@ -3,5 +3,5 @@
 git pull origin main
 
 docker build -t cali-web-prototype .
-docker container rm $(docker container ls -aq)
+docker container rm -f $(docker container ls -aq)
 docker run -p 3000:3000 cali-web-prototype
