34765c0605a1e487215a81a774fdcc16ee30d348
Yi Hong-ju <to_hongju@pm.me>
2025-02-21 21:58:56 +0900

    Fix installing deprecated version of Python

--- search_engine/run_dev.sh
+++ search_engine/run_dev.sh
@@ -4,7 +4,11 @@ export MEILI_MASTER_KEY=x0tii5RDPBcoP7PX  # Random 16 bytes base64 value
 sh -c /bin/meilisearch &
 MEILISEARCH_PID=$!
 
-apk add python3=3.12.8-r1 py3-pip
+# Intended python3=3.12.9-rX (Latest revision)
+apk add \
+    --repository http://dl-cdn.alpinelinux.org/alpine/v3.21/main \
+    python3 \
+    py3-pip
 pip install -r /cali/search_engine/requirements.txt --break-system-packages
 
 export PYTHONPATH=/cali
