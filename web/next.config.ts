a636af1c7f3a6dd472050c21687f5815bb244263
Yi Hong-ju <to_hongju@pm.me>
2025-02-22 19:32:17 +0900

    Add .env file reading code to next.config.js

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,4 +1,15 @@
+import * as path from "path";
+
+import * as dotenv from "dotenv";
 import type { NextConfig } from "next";
+
+const dotenvFile = process.env.DOTENV_FILE;
+
+if (!dotenvFile) {
+  throw new Error("DOTENV_FILE environment variable is not defined.");
+}
+
+dotenv.config({ path: path.resolve(__dirname, "..", dotenvFile) });
 
 const nextConfig: NextConfig = {
   /* config options here */
